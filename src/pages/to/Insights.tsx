9866e66e1ebc1d853c541cc6c3bdaf7d4e50b0b1
Mir Saheb Ali <mirsahebali204@gmail.com>
2023-03-27 19:02:27 +0530

    done with filters

--- src/pages/to/Insights.tsx
+++ src/pages/to/Insights.tsx
@@ -3,12 +3,12 @@ import clientPromise from "@/lib/mongodb";
 import Link from "next/link";
 import Card from "../components/Card";
 import { Data } from "@/types";
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import styles from "@/styles/Insights.module.css"
+interface Prop {
+  data: { stats: Data[] };
 
-interface Prop{
-data: {stats: Data[]}
-
-func: Function
+  func: Function;
 }
 
 export async function getServerSideProps() {
@@ -27,58 +27,89 @@ export async function getServerSideProps() {
     console.error(e);
   }
 }
+
+
 export default function Insights({ data }: Prop) {
-  const [pr, setPr] = useState("country")
+  const [pr, setPr] = useState("country");
   const insightsData: Data[] = data.stats;
-  const [filteredList, setfilteredList] = useState(insightsData)
-const [filterText, updateFilterText] = useState<string>("")
-const [fList, updatefList] = useState(insightsData)
-function getData(d:any[], prop:string){
-let newVal = d.map((curr:any)=> {
-return curr[prop]
-})
- 
- return newVal = ["All", ...new Set(newVal)] 
-}
-const arrayProps:string[] = ["country", "region", "end_year","title", "topic", "sector", "insight"]
-
-let filteredData = insightsData.map((item:any)=> item[pr])
-filteredData = [...new Set(filteredData)]
+  const [filteredList, setfilteredList] = useState(insightsData);
+  const [filterText, updateFilterText] = useState<string>("");
+  const [fList, updatefList] = useState(insightsData);
+  let arr: any[] = []
 
 
-function onFilterValueSelectedProp(filterValue:any){
-setPr(filterValue)
-}
 
-function returnData(value:any){
- let currData = insightsData.filter((item:any)=> item[pr] === value)
-return currData
-}
+  const arrayProps: string[] = [
+    "All",
+    "country",
+    "region",
+    "end_year",
+    "topic",
 
-function onFilterValueSelected(filterValue:any){
-  updateFilterText(filterValue);
-updatefList(returnData(filterText))
-
-}
+  ];
 
 
+
+  function onFilterValueSelectedProp(filterValue: any) {
+    setPr(filterValue);
+    
+  }
+
+  function onFilterValueSelected(filterValue: any) {
+   
+    updateFilterText(filterValue);
+    
+  }
+
+  function arrayMaker(a: any, b: any): any[] {
+    let arr = []
+    arr = insightsData.filter((obj: any) => obj[a] != null && obj[a] != "" && obj[a] != undefined).reduce((accumulator: any, current: any) => {
+      let existing = accumulator.find((item: any) => item[a] === current[a]
+
+      );
+      if (existing) {
+        existing[b] += parseInt(current[b])
+      } else {
+
+        accumulator.push(
+          {
+            [a]: current[a],
+            [b]: current[b]
+          })
+      }
+      return accumulator
+    }, [])
+
+    return arr
+  }
+useEffect( ()=> {
+  if (pr === "All")
+    updatefList(insightsData)
+  else
+  updatefList(insightsData.filter((item: any) => item[pr] === filterText))
+
+} , [filterText, insightsData, pr])
   return (
     <div className="flex ">
       <Sidebar />
       <div>
         <div className="text-2xl font-bold">Insights</div>
-       <Filter data={fList} p={`${pr}`} ar={arrayProps} filterValueSelectedProp={onFilterValueSelectedProp} filterValueSelected={onFilterValueSelected}/>
+        <Filter
+          data={arrayMaker(`${pr}`, `intensity`)}
+          p={`${pr}`}
+          ar={arrayProps}
+          filterValueSelectedProp={onFilterValueSelectedProp}
+          filterValueSelected={onFilterValueSelected}
+        />
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 ">
-          {filteredList.map((data: any) => {
+          {fList.map((data: any) => {
             return (
               <div key={data._id} className="m-2">
-                <Link href={data.url}>
-                  <Card
-                    title={data.title}
-                    country={data.country}
-                    region={data.region}
-                  />
-                </Link>
+                <Card
+                  title={data.title}
+                  country={data.country}
+                  region={data.region}
+                />
               </div>
             );
           })}
@@ -88,34 +119,58 @@ updatefList(returnData(filterText))
   );
 }
 
-interface prop{
-data: any[],
-p: string,
-ar: string[],
-filterValueSelectedProp: Function,
-filterValueSelected: Function
+interface prop {
+  data: any[];
+  p: string;
+  ar: string[];
+  filterValueSelectedProp: Function;
+  filterValueSelected: Function;
 }
 
-function Filter({data, p, ar,  filterValueSelectedProp, filterValueSelected}:prop){
-  function onFilterValueChange(event:any){
-  filterValueSelectedProp(event.target.value)
+function Filter({
+  data,
+  p,
+  ar,
+  filterValueSelectedProp,
+  filterValueSelected,
+}: prop) {
+  function onFilterValueChange(event: any) {
+    filterValueSelectedProp(event.target.value);
   }
-function onFilterValueSelected(event:any){
-  filterValueSelected(event.target.value);
+  function onFilterValueSelected(event: any) {
+    filterValueSelected(event.target.value);
+  }
+
+  return (
+    <div className={styles.select}>
+      <select className={styles._select}
+        name={`props`}
+        id={`props`}
+        title={`props`}
+        onChange={onFilterValueChange}
+      >
+        {ar.map((item: any, index) => {
+          return (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+      <select className={styles.select}
+        name={`${p}`}
+        id={`${p}`}
+        title={`${p}`}
+        onChange={onFilterValueSelected}
+      >
+        {data.map((item: any, index) => {
+          return (
+            <option key={index} value={item[p]}>
+              {item[p]}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 }
-
-return <div>
-  <select name={`props`} id={`props`} title={`props`} onChange={onFilterValueChange}>
-{ar.map((item: any, index)=>{
-return <option key={index} value={item}>{item}</option>
-})}
-</select>
-<select name={`${p}`} id={`${p}`}title={`${p}`} onChange={onFilterValueSelected}>
-{data.map((item: any, index)=>{
-return <option key={item._id} value={item[p]}>{item[p]}</option>
-})}
-</select>
-</div>
-}
-
-
